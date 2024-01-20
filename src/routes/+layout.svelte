671d6d18f324dba8def447b0419d93150577dc59
Jxk0be <jakeshoffner27@gmail.com>
2024-01-20 00:05:59 -0500

    navbar sizing

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../app.css";
+</script>
+
+<div class="fixed w-full h-[70px] bg-red-400">
+    <div class="max-w-[1500px] m-auto w-full bg-black h-full">
+
+    </div>
+</div>
+
+<slot />
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+
+</script>
+
+<h1>Contact page</h1>

1b836d5a70bda6370ae00dfe9ca249f1d1b4b68d
Jxk0be <JakeShoffner27@gmail.com>
2024-01-23 08:51:56 -0500

    TODO: comments added

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -2,11 +2,13 @@
     import Image from "$lib/images/linkedin_bg.jpg";
 </script>
 
-<div class="h-screen w-full bg-blue-400 flex justify-between items-center flex-col md:flex-row">
-    <div class="flex justify-center items-center w-1/2 text-[50px] font-bold">
+<!-- TODO: Change the Hero page to have the image as the background 
+     with a dark filter over it to see the text properly (YT Val video for reference) -->
+<div class="h-screen w-full bg-blue-400 flex justify-around md:justify-between items-center flex-col md:flex-row">
+    <div class="flex justify-center whitespace-nowrap items-center w-full md:w-1/2 text-[50px] font-bold">
         <h1>Fusion Auto Lab</h1>
     </div>
-    <div class="w-1/2">
+    <div class="md:w-1/2 w-full">
         <img src={Image} alt="Hero Page for Fusion Auto Lab" />
     </div>
 </div>
